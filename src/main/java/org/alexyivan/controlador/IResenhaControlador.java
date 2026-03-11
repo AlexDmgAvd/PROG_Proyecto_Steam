9509d30e6b4caf5b6714237043eefc9919b29aee
kaisquest <ivan.fndz.lopez@gmail.com>
2026-03-11 21:53:30 +0100

    Merge conflict resolve

--- src/main/java/org/alexyivan/controlador/IResenhaControlador.java
+++ src/main/java/org/alexyivan/controlador/IResenhaControlador.java
@@ -2,23 +2,23 @@ package org.alexyivan.controlador;
 
 import org.alexyivan.exception.ValidacionException;
 import org.alexyivan.modelo.dto.EstadisticasResenhaDTO;
-import org.alexyivan.modelo.dto.ResenhaDTO;
+import org.alexyivan.modelo.dto.ResenhaDto;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface IResenhaControlador {
 
-    Optional<ResenhaDTO> escribirResenha() throws ValidacionException;
+    Optional<ResenhaDto> escribirResenha() throws ValidacionException;
 
     boolean eliminarResenha() throws ValidacionException;
 
-    List<ResenhaDTO> verResenhasJuego() throws ValidacionException;
+    List<ResenhaDto> verResenhasJuego() throws ValidacionException;
 
     boolean ocultarResenha(long idUsuario, long idResenha) throws ValidacionException;
 
     Optional<EstadisticasResenhaDTO> consultarEstadisticas() throws ValidacionException;
 
-    List<ResenhaDTO> verResenhaUsuario (long idUsuario);
+    List<ResenhaDto> verResenhaUsuario (long idUsuario);
 
 }
--- src/main/java/org/alexyivan/controlador/ResenhaControlador.java
+++ src/main/java/org/alexyivan/controlador/ResenhaControlador.java
@@ -1,7 +1,13 @@
 package org.alexyivan.controlador;
 
 
+import org.alexyivan.exception.ValidacionException;
+import org.alexyivan.modelo.dto.EstadisticasResenhaDTO;
+import org.alexyivan.modelo.dto.ResenhaDto;
 import org.alexyivan.repositorio.interfaces.IResenhaRepo;
+
+import java.util.List;
+import java.util.Optional;
 
 public class ResenhaControlador implements IResenhaControlador {
 
@@ -11,4 +17,33 @@ public class ResenhaControlador implements IResenhaControlador {
         this.resenhaRepo = resenhaRepo;
     }
 
+    @Override
+    public Optional<ResenhaDto> escribirResenha() throws ValidacionException {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean eliminarResenha() throws ValidacionException {
+        return false;
+    }
+
+    @Override
+    public List<ResenhaDto> verResenhasJuego() throws ValidacionException {
+        return List.of();
+    }
+
+    @Override
+    public boolean ocultarResenha(long idUsuario, long idResenha) throws ValidacionException {
+        return false;
+    }
+
+    @Override
+    public Optional<EstadisticasResenhaDTO> consultarEstadisticas() throws ValidacionException {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<ResenhaDto> verResenhaUsuario(long idUsuario) {
+        return List.of();
+    }
 }
